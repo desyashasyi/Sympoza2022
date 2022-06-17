c72c16ca8ea65f8e15896b061ede107675eb9ccc
hafizhass <106043761+hafizhass@users.noreply.github.com>
2022-06-18 05:51:13 +0700

    Add files via upload

---
+++ Modules/News/Resources/views/livewire/berita3.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <h3>TEMPO.CO, Jakarta - Rifky Bujana, siswa yang baru saja lulus dari SMAN 28 Jakarta, berhasil lolos sebagai penerima 
+        Beasiswa Indonesia Maju (BIM) di University of British Columbia--universitas tiga terbaik di Kanada dan menyatakan 
+        diri dalam 20 peringkat tertinggi di dunia. Anak dari seorang pekerja ojek online ini membawa serta sederet catatan 
+        prestasi di bidang fisika dan robotik selama duduk di bangku SMA.Rifky di antaranya meraih medali emas di Kompetisi 
+        Robot Nusantara (KRON) Tahun 2019 kategori Creative dan Juara 2 Thamrin Olympiad and Cup Kategori Robotic Competition. 
+        Rifky juga Juara 1 Animasi VI Robotic Competition 2020, Runner Up Gameloft Student Gameday Competition, dan memiliki 
+        Silver Medal KoPSI 2021 Kategori FTR (Fisika Terapan dan Rekayasa). Selama masa pendidikannya, ia memperoleh Kartu 
+        Jakarta Pintar (KJP) yang merupakan bantuan pemerintah bagi siswa kurang mampu. Rifky memanfaatkannya untuk membeli 
+        peralatan sekolah seperti seragam, buku, dan lain-lain. Hal ini memperingan beban ayahnya yang harus mengalokasikan 
+        pendapatan untuk uang pendidikan Rifky serta kebutuhan sehari-hari.
+        Rifky Bujana, putra dari driver Ojol yang berhasil lolos sebagai penerima Beasiswa Indonesia Maju (BIM) di University
+        of British Columbia (UBC). asional.kemdikbud.go.id Dikutip dari situs web Pusat Prestasi Nasional, Kementerian Pendidikan
+        Kebudayaan dan Ristek, Rifky bercerita kalau dirinya sebenarnya tak berani bermimpi bisa berkuliah di UBC, atau kampus 
+        manapun di luar negeri. Tapi, kehadiran dan tawaran Beasiswa Indonesia Maju dari Kementerian Pendidikan mengubah 
+        keberaniannya. BIM adalah program bagi peserta didik/lulusan berprestasi yang berkeinginan melanjutkan studi di perguruan 
+        tinggi impian baik di dalam maupun luar negeri.</h3>
+</div>
